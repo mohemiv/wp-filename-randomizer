d8527c9d3d4b98a3218b1f512dd065e51ef746ef
Arseniy Sharoglazov <mohemiv@users.noreply.github.com>
2022-08-06 21:21:56 +0300

    Enforcing 16-character prefix for Windows systems

--- filename-randomizer.php
+++ filename-randomizer.php
@@ -3,7 +3,7 @@
  * Plugin Name:  Filename Randomizer
  * Description:  Filename Randomizer adds randomization to file names when uploading files into WordPress.
  * Plugin URI:   https://github.com/mohemiv/wp-filename-randomizer
- * Version:      1.0.1
+ * Version:      1.0.2
  * Requires PHP: 7.0
  * License:      ISC License (ISC)
  * License URI:  https://opensource.org/licenses/ISC
@@ -13,14 +13,19 @@
 
 defined('ABSPATH') or die();
 
+if (strtoupper(substr(PHP_OS, 0, 3)) == "WIN") {
+    define("FILENAME_RANDOMIZER_RANDOMIZE_LEN", 16);
+} else {
+    define("FILENAME_RANDOMIZER_RANDOMIZE_LEN", 8);
+}
 
 function filename_randomizer__randomize_name($filename) {
-    if (preg_match("/^[a-f0-9]{8}-.*/", $filename)) {
+    if (preg_match('/^[a-f0-9]{'. FILENAME_RANDOMIZER_RANDOMIZE_LEN . '}-.*/', $filename)) {
         return $filename;
     }
 
     $key = sha1(random_bytes(32));
-    $key = substr($key, 0, 8);
+    $key = substr($key, 0, FILENAME_RANDOMIZER_RANDOMIZE_LEN);
 
     return "$key-$filename";
 }
