467c534cd73f8b072dababf9483a8103328748e7
Arseniy Sharoglazov <mohemiv@users.noreply.github.com>
2022-08-06 17:15:25 +0300

    Excluding .gitignore and build.sh from builds

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Building v$VERSION release"
 pushd $SCRIPT_DIR
 rm filename-randomizer-v$VERSION.zip
 mkdir filename-randomizer
-cp `git ls-files` ./filename-randomizer
-zip -r filename-randomizer-v$VERSION.zip ./filename-randomizer
+cp -r `git ls-files` ./filename-randomizer
+zip -r filename-randomizer-v$VERSION.zip ./filename-randomizer -x ./filename-randomizer/.gitignore -x ./filename-randomizer/build.sh
 rm -rf filename-randomizer
 popd
